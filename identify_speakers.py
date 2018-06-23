1a1534d7b1cab9fc7d648fd78588972c4c09142b
scott <scott@localhost.localdomain>
2018-06-22 18:46:35 -0600

    added another data point, refined regex to catch more speakers

--- identify_speakers.py
+++ identify_speakers.py
@@ -9,19 +9,22 @@ import pprint
 ###
 
 #finds speakers in the majorityy of documents, 5058/5137 total documents have at least 1 match
-speaker = re.compile(r'(\n(Mr\.|Mrs\.|An Hon\. Member|Premier|Hon\.|Ms\.|Some).*?:\s*(—|–|-{1,2}| |)\s*)', re.UNICODE|re.IGNORECASE)
+speaker = re.compile(r'(\n(his honour|her honour|mr\.|mrs\.|an hon\. member|premier|hon\.|ms\.|some).*?:\s*(─|—|–|-{1,2}| |)\s*)', re.UNICODE|re.IGNORECASE)
 
 
 #for the remaning ~100 or so documents that have a different formatting for speakers
 #e.g.:
 #MR. A. THIBAULT: (Melfort-Kinistino)
 #SOME HON. MEMBERS:
-speaker_alternative = re.compile(r'((\n(mr|some|hon|mrs|ms).*?:\s*)(\(*.+\))?)', re.UNICODE|re.IGNORECASE)
+speaker_alternative = re.compile(r'((\n(his honour|her honour|mr|some|hon|mrs|ms).*?:\s*)(\(*.+\))?)', re.UNICODE|re.IGNORECASE)
+
 
 data_directory = './hansard_txts/'
-total_files = 0
-hansards_with_speakers = 0
-hansards_without_speakers = []
+total_files = 0 #keep track of how many files we've looked at
+hansards_with_speakers = 0 #increment if we find any speaker at all in a file
+hansards_without_speakers = [] #append to this list hansard files that we find 0 speakers in
+total_speaking_parts_identified = 0 #increment this for every speaking part we find
+
 for filename in os.listdir(data_directory):
     #if filename.startswith('19'):
         total_files += 1
@@ -36,22 +39,39 @@ for filename in os.listdir(data_directory):
 
         if (found_counter != 0):
             hansards_with_speakers += 1
+            total_speaking_parts_identified += found_counter
+
+        #if (found_counter == 1):
+          #print("Found: " + str(found_counter) + " in " + filename)
+          #for found in re.findall(speaker, text):
+            #print(found[0])
 
         #second pass using alternative regex
-        #show text of hansards that found 0 speakers with the speaker regex
         if (found_counter==0):
           for found in re.findall(speaker_alternative, text):
             found_counter += 1
             print(found[0])
             #print("Found: " + str(found_counter) + " in " + filename)
+
+
+          #if (found_counter == 2):
+            #print("Found: " + str(found_counter) + " in " + filename)
+            #print(text)
+            #for found in re.findall(speaker_alternative, text):
+              #print(found[0])
+ 
           if (found_counter != 0):
             hansards_with_speakers += 1
+            total_speaking_parts_identified += found_counter
+
           if (found_counter == 0):
             #print(text)
             hansards_without_speakers.append(filename)
-
+        
 
 print("\n" + str(hansards_with_speakers) + " out of " + str(total_files) + " have speakers in them \n")
+
+print("\n" + str(total_speaking_parts_identified) + " total speaking parts identified \n")
 
 print("These " + str(len(hansards_without_speakers)) + " files currently have 0 speakers detected in them: \n")
 
